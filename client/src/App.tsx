75778cc8ed42c9b24a0c5f806cebc059e02bb0fa
Tanish <tanishtp01@gmail.com>
2025-05-05 16:57:33 +0530

    test

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,8 @@
 import './App.css'
-import { BrowserRouter, Routes, Route,  } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, } from 'react-router-dom'
 import Login from './Pages/login'
 import Signup from './Pages/signup'
-import HomeLayout from './layout/Home';
+import HomeLayout from './layout/HomeLayout';
 import Create_blog from './layout/BlogCreate';
 import Blogs from './layout/Blogs';
 import Account from './layout/UserAccount';
@@ -12,18 +12,18 @@ import { SessionProvider } from './context/session';
 function App() {
   return (
     <div>
-     <BrowserRouter>
-     <SessionProvider>
-      <Routes>
-        <Route path="/" element={<HomeLayout />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/create_blog" element={<Create_blog />} />
-        <Route path="/blog" element={<Blogs />} />
-        <Route path="/account" element={<Account />} />
-        <Route path="/openblog/:id" element={<Openblog />} />
-      </Routes>
-      </SessionProvider>
+      <BrowserRouter>
+        <SessionProvider>
+          <Routes>
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<HomeLayout />} />
+            <Route path="/create_blog" element={<Create_blog />} />
+            <Route path="/blog" element={<Blogs />} />
+            <Route path="/account" element={<Account />} />
+            <Route path="/openblog/:id" element={<Openblog />} />
+          </Routes>
+        </SessionProvider>
       </BrowserRouter>
     </div>
   )
